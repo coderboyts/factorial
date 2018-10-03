0dfadb3a589789607e11445826ceef1e2758966a
tarun-sharma03 <tarundixit0310@gmail.com>
2018-10-03 21:41:06 +0530

    first commit

--- factorial.c
+++ factorial.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-	int a,x,c=1;
+	int a,x,d=1;
 	printf("Enter the value of a : ");
 	scanf("%d",&a);
 	for(x=1;x<=a;x++)
 	{
-		c=x*c;
+		d=x*d;
 		
 		
-	} printf("%d",c);
+	} printf("%d",d);
 }

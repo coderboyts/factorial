5b44be3497daa09d70902f445ad492e61480f142
Soumel Zutshi <soumelzutshi@gmail.com>
2018-10-14 13:56:28 +0530

    Factorial by recursion
    Try using function for factorial as they are very easy to do.

--- factorial.c
+++ factorial.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
+#include<stdlib.h>
+/*The program is to calculate a factorial of a number by using recursion*/
+int fact(int);//Funtion declaration
 int main()
 {
-
-	int a,x,d=1;
-	printf("Enter the value of a : ");
-
-	scanf("%d",&a);
-	for(x=1;x<=a;x++)
-	{
-		d=x*d;
-		
-		
-	} printf("The factorial is:%d",c);
+	int no,ans;
+	printf("Enter a number:");
+	scanf("%d",&no);
+	ans=fact(no);//Function calling
+	printf("Factorial is %d",ans);
+}
+int fact(int x)//Function defining
+{
+	int f;
+	if(x==0)
+		return 1;
+	else
+		f=x*fact(x-1);//Recursion
+	return f;
 }
